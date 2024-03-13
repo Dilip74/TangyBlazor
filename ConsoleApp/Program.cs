6a7346c5d7c7032791c42312c8309e762bea12c9
Dilip Kumar <dilipkumarr04@gmail.com>
2024-03-13 12:40:55 +0530

    added console app 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace ConsoleApp
+{
+    public string str1 = "Dilip";
+
+    Console.WriteLine("Hello, World!");
+}
